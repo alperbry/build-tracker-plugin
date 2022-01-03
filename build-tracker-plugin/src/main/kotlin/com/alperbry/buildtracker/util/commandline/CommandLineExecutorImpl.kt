e4c0505a29bd5a3973f95e6e597a5b40943af0dc
Alper Boray <alper.boray@monstar-lab.com>
2022-01-03 10:56:14 +0100

    test: Add unit tests for CommandLineExecutor

--- build-tracker-plugin/src/main/kotlin/com/alperbry/buildtracker/util/commandline/CommandLineExecutorImpl.kt
+++ build-tracker-plugin/src/main/kotlin/com/alperbry/buildtracker/util/commandline/CommandLineExecutorImpl.kt
@@ -1,19 +1,39 @@
 package com.alperbry.buildtracker.util.commandline
 
+import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.process.ExecSpec
 import java.io.ByteArrayOutputStream
 
 class CommandLineExecutorImpl(
-    private val project: Project
+    private val project: Project,
+    private val actionFactory: CommandLineExecSpecActionFactory = CommandLineExecSpecActionFactory()
 ) : CommandLineExecutor {
 
     override fun execute(vararg cmd: String): String {
         val outputStream = ByteArrayOutputStream()
+        val execSpec = actionFactory.create(cmd, outputStream)
+        project.exec(execSpec)
 
-        project.exec {
-            it.setCommandLine(*cmd)
-            it.standardOutput = outputStream
-        }
         return outputStream.toString()
     }
+}
+
+class CommandLineExecSpecAction(
+    private val commands: Array<out String>,
+    private val outputStream: ByteArrayOutputStream
+) : Action<ExecSpec> {
+
+    override fun execute(execSpec: ExecSpec) {
+        execSpec.setCommandLine(commands)
+        execSpec.standardOutput = outputStream
+    }
+}
+
+class CommandLineExecSpecActionFactory {
+
+    fun create(
+        commands: Array<out String>,
+        outputStream: ByteArrayOutputStream
+    ): Action<ExecSpec> = CommandLineExecSpecAction(commands, outputStream)
 }

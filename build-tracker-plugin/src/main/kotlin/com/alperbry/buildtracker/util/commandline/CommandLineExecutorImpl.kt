bf820a29a145eea99a2e6438605057db84dbd9a1
Alper Boray <alper.boray@monstar-lab.com>
2022-01-03 10:56:14 +0100

    test: Add unit tests for CommandLineExecutor

--- build-tracker-plugin/src/main/kotlin/com/alperbry/buildtracker/util/commandline/CommandLineExecutorImpl.kt
+++ build-tracker-plugin/src/main/kotlin/com/alperbry/buildtracker/util/commandline/CommandLineExecutorImpl.kt
@@ -1,19 +1,39 @@
 package com.alperbry.buildtracker.util.commandline
 
+import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.process.ExecSpec
 import java.io.ByteArrayOutputStream
 
 class CommandLineExecutorImpl(
-    private val project: Project
+    private val project: Project,
+    private val actionFactory: CommandLineExecSpecActionFactory = CommandLineExecSpecActionFactory()
 ) : CommandLineExecutor {
 
     override fun execute(vararg cmd: String): String {
         val outputStream = ByteArrayOutputStream()
+        val execSpec = actionFactory.create(outputStream, *cmd)
+        project.exec(execSpec)
 
-        project.exec {
-            it.setCommandLine(*cmd)
-            it.standardOutput = outputStream
-        }
         return outputStream.toString()
     }
+}
+
+class CommandLineExecSpecAction(
+    private val commands: List<String>,
+    private val outputStream: ByteArrayOutputStream
+) : Action<ExecSpec> {
+
+    override fun execute(execSpec: ExecSpec) {
+        execSpec.commandLine = commands
+        execSpec.standardOutput = outputStream
+    }
+}
+
+class CommandLineExecSpecActionFactory {
+
+    fun create(
+        outputStream: ByteArrayOutputStream,
+        vararg commands: String,
+    ): Action<ExecSpec> = CommandLineExecSpecAction(commands.toList(), outputStream)
 }
--- build-tracker-plugin/src/test/kotlin/com/alperbry/buildtracker/util/commandline/CommandLineExecutorUnitTest.kt
+++ build-tracker-plugin/src/test/kotlin/com/alperbry/buildtracker/util/commandline/CommandLineExecutorUnitTest.kt
@@ -1,4 +1,86 @@
 package com.alperbry.buildtracker.util.commandline
 
+import io.kotest.matchers.types.shouldBeInstanceOf
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.gradle.api.Project
+import org.gradle.process.ExecSpec
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.io.ByteArrayOutputStream
+
+@ExtendWith(MockKExtension::class)
 class CommandLineExecutorUnitTest {
+
+    @RelaxedMockK
+    lateinit var project: Project
+
+    @MockK
+    lateinit var actionFactory: CommandLineExecSpecActionFactory
+
+    @MockK
+    lateinit var action: CommandLineExecSpecAction
+
+    private lateinit var executor: CommandLineExecutor
+
+    @BeforeEach
+    fun setup() {
+        executor = CommandLineExecutorImpl(project, actionFactory)
+
+        every { actionFactory.create(any(), any(), any()) } returns action
+    }
+
+    @Test
+    fun `When execute is called, it executes the commands with the help of CommandLineExecSpecActionFactory`() {
+        // Given
+        val command = "command"
+        val option = "-n"
+
+        // When
+        executor.execute(command, option)
+
+        // Then
+        verify {
+            actionFactory.create(any(), command, option)
+            project.exec(action)
+        }
+    }
+
+    @Test
+    fun `When CommandLineExecSpecAction#execute is called, it sets given commands and outputStream to the ExecSpec`() {
+        // Given
+        val execSpec = mockk<ExecSpec>(relaxed = true)
+        val command = "command"
+        val option = "-n"
+        val outputStream = ByteArrayOutputStream()
+        val action = CommandLineExecSpecAction(listOf(command, option), outputStream)
+
+        // When
+        action.execute(execSpec)
+
+        // Then
+        verify {
+            execSpec.commandLine = listOf(command, option)
+            execSpec.standardOutput = outputStream
+        }
+    }
+
+    @Test
+    fun `When create of CommandLineExecSpecActionFactory is called, it creates expected CommandLineExecSpecAction`() {
+        // Given
+        val commands = arrayOf<String>()
+        val outputStream = ByteArrayOutputStream()
+        val factory = CommandLineExecSpecActionFactory()
+
+        // When
+        val result = factory.create(outputStream, *commands)
+
+        // Then
+        result.shouldBeInstanceOf<CommandLineExecSpecAction>()
+    }
 }

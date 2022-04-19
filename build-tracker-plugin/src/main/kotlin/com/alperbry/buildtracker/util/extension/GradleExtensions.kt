a12d366f0ee305b7ed4e51a58af9853a6c14ec22
Alper Boray <alper.boray@monstar-lab.com>
2022-04-19 11:28:42 +0200

    fix: Fix afterEachSubProjectEvaluated extension to wait for subproject evaluation

--- build-tracker-plugin/src/main/kotlin/com/alperbry/buildtracker/util/extension/GradleExtensions.kt
+++ build-tracker-plugin/src/main/kotlin/com/alperbry/buildtracker/util/extension/GradleExtensions.kt
@@ -3,8 +3,8 @@ package com.alperbry.buildtracker.util.extension
 import org.gradle.api.Project
 
 internal inline fun Project.afterEachSubProjectEvaluated(crossinline block: (Project) -> Unit) {
-    subprojects {
-        afterEvaluate { evaluatedChild ->
+    subprojects { subproject ->
+        subproject.afterEvaluate { evaluatedChild ->
             block(evaluatedChild)
         }
     }
